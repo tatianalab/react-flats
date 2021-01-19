bfa08cd374e1b5eea4a8be75811932148c2f14f5
Tatiana Labrousse <mvtatianalabrousse@gmail.com>
2021-01-19 09:52:24 -0500

    replaced google maps with mapbox api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.scss';
-import GoogleMapReact from 'google-map-react';
+import ReactMapGL from 'react-map-gl'
 
 
 import Flat from './components/Flat'
@@ -18,10 +18,6 @@ class App extends React.Component {
 
 
   render(){
-    const center = {
-      Lat:48.8566,
-      Lng:2.3522
-    }
 
     const flatItems = this.state.allFlats.map(item => <Flat key={item.id} item={item} />)
 
@@ -32,11 +28,7 @@ class App extends React.Component {
       </div>
 
       <div className="map-container">
-      <GoogleMapReact
-          center={center}
-          Zoom={11}
-        >
-        </GoogleMapReact>
+      <Map />
       </div>
       </>
       )
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
+import ReactMapGL from 'react-map-gl'
 
-class Map extends React.Component {
-  render(){
-    return (
-      <div>
-      <h1>This is a Google map</h1>
-      </div>
-      )
-  }
+function Map (){
+  const REACT_APP_MAPBOX_ACCESS_TOKEN="pk.eyJ1IjoidGF0aWFsYWIiLCJhIjoiY2toNTFtemxjMGNjbDJzcXZyOHlqMDFyZCJ9.wahnCxzAg2kn5MRNnp3AwQ"
+
+  const [viewport, setViewport] = useState ({
+    latitude: 48.8566,
+    longitude: 2.3522,
+    width:"100%",
+    height:"100vh",
+    zoom:10
+  });
+
+  return (
+
+    <ReactMapGL {...viewport}
+    mapboxApiAccessToken = {REACT_APP_MAPBOX_ACCESS_TOKEN}
+    mapStyle = "mapbox://styles/tatialab/ckk43xehh3b5k17mjbaipfsis"
+    onViewportChange= {viewport => {
+    setViewport(viewport);
+}}
+     >
+      Markers
+    </ReactMapGL>
+
+    )
 }
 
 export default Map
+
+
